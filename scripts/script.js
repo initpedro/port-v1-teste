8e0c5adc536c3a3f21fa2ca05ae3ca2ddb4b0594
Pedro Fonseca <132118855+pedrowpy@users.noreply.github.com>
2024-10-16 15:53:22 -0300

    .js att 😎

--- scripts/script.js
+++ scripts/script.js
@@ -2,16 +2,4 @@ window.addEventListener("load", function () {
   document.body.classList.add("loaded")
 })
 
-const myObserver = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add("show")
-    } else {
-      entry.target.classList.remove("show")
-    }
-  })
-})
-
-const elements = document.querySelectorAll(".hidden")
-
-elements.forEach((element) => myObserver.observe(element))
+audioElement.play()
